fff6d3b18bda6ac06da266bdaf8d9dddc52bb04a
yashi8484 <kobayashi@giftee.co>
2019-10-26 16:48:29 +0900

    Add initial Vue application

---
+++ vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>This is Svelte x {{ name }} page</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Vue"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
