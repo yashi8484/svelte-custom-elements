fbbdeffea33ef0be9c8e482796df01dee6c2b32c
yashi8484 <kobayashi@giftee.co>
2019-10-27 17:17:27 +0900

    Add custom elements to React application

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../public/svelte-components";
 
 const HelloMessage = props => {
-  return <div>This is Svelte x {props.name} page</div>;
+  return (
+    <>
+      <div>This is Svelte x {props.name} page</div>
+      <my-component />
+    </>
+  );
 };
 
-var mountNode = document.getElementById('app');
+var mountNode = document.getElementById("app");
 ReactDOM.render(<HelloMessage name="React" />, mountNode);
