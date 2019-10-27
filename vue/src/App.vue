b2e0362c11733a8b4afb8b002a6370c68414f00a
yashi8484 <kobayashi@giftee.co>
2019-10-27 18:34:55 +0900

    Add svelte-card component to Vue application

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <div>This is Svelte x {{ name }} page</div>
-    <my-component />
+  <div class="container">
+    <h1>This is Svelte x {{ name }} page</h1>
+    <div class="content-wrapper">
+      <template v-for="card in cards">
+        <svelte-card
+          :title="card.title"
+          :message="card.message"
+          :key="card.title"
+        />
+      </template>
+    </div>
   </div>
 </template>
 
@@ -11,11 +19,24 @@ import "../public/svelte-components";
 export default {
   data() {
     return {
-      name: "Vue"
+      name: "Vue",
+      cards: [...Array(10)].map((_, i) => ({
+        title: `Card in Vue ${i + 1}`,
+        message: `This is svelte-card element ${i + 1}`
+      }))
     };
   }
 };
 </script>
 
 <style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.content-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
 </style>
