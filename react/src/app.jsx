82e46394df0eeb3267798814c728eeaf8a7606e0
yashi8484 <kobayashi@giftee.co>
2019-10-28 00:52:30 +0900

    Add svelte-button component to React application

--- react/src/app.jsx
+++ react/src/app.jsx
@@ -3,16 +3,25 @@ import "./app.css";
 import "../public/svelte-components";
 
 export const App = props => {
+  function handleButtonOnClick(e) {
+    alert("button clicked!");
+  }
+
   return (
     <div className="container">
-      <h1>This is Svelte x {props.name} page</h1>
+      <h1>Svelte x {props.name}</h1>
       <div className="content-wrapper">
         {[...Array(10)].map((_, i) => (
           <svelte-card
             title={`Card in React ${i + 1}`}
-            message={`This is svelte-card element ${i + 1}`}
+            message={`svelte-card element ${i + 1}`}
             key={i}
-          />
+          >
+            <svelte-button
+              slot="footer"
+              onClick={handleButtonOnClick}
+            ></svelte-button>
+          </svelte-card>
         ))}
       </div>
     </div>
